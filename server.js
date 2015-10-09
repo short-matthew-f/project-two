ee70641df6a6a7b107662da5729ef6e4fa7eecdb
Matthew F. Short <short.matthew.f@gmail.com>
2015-10-09 10:39:27 -0400

    added mongo_uri stuff

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express'),
     PORT    = process.env.PORT || 5432,
-    server  = express();
+    server  = express(),
+    MONGOURI = process.env.MONGOLAB_URI,
+    dbname   = "please_change_this",
+    mongoose = require('mongoose');
 
 server.get('/super-secret-test', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 });
 
+mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function () {
   console.log("SERVER IS UP ON PORT:", PORT);
 });
